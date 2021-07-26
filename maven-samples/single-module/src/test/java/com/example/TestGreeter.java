4bd5b01ff70727d961ea2dd0a7675837a36a4173
varun <varundwivedi410@gmail.com>
2021-07-26 14:06:37 +0530

    This is second commit

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
   @Test
   public void greetShouldIncludeGreetingMessages() {
-    String someone = "World, Thanks you making me Happy again";
+    String someone = "World, Thanks you making me very very Happy ";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
