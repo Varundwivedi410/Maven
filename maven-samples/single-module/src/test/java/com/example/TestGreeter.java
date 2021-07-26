c8bebe4dbb5073f1e94684b611b30efdacfe10ef
varun <varundwivedi410@gmail.com>
2021-07-26 13:55:56 +0530

    This is first maven commit

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  @Test
+  public void greetShouldIncludeGreetingMessages() {
+    String someone = "World, Thanks you making me Happy again";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
